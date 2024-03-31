e4cda898f98d7027134b0258a95284eee499f64f
AlexisLavigne <alexis.lavigne@polymtl.ca>
2024-03-31 16:23:25 -0400

    AL: modified waffle chart data format

--- src/App.js
+++ src/App.js
@@ -3,14 +3,54 @@ import MyD3Component from './MyD3Component.js';
 import WaffleChart from './WaffleChart.js';
 import './App.css';
 
-const data = [
-  { nationality: "English", count: 20 },
-  { nationality: "German", count: 24 },
-  { nationality: "French", count: 12 },
-  { nationality: "Brazilian", count: 20 },
-  { nationality: "Italian", count: 6 },
-  { nationality: "American", count: 7 }
-];
+const waffle_data = [
+  [ 'British', 165 ],
+  [ 'American', 158 ],
+  [ 'Italian', 99 ],
+  [ 'French', 73 ],
+  [ 'German', 50 ],
+  [ 'Brazilian', 32 ],
+  [ 'Argentine', 24 ],
+  [ 'Swiss', 23 ],
+  [ 'Belgian', 23 ],
+  [ 'South African', 23 ],
+  [ 'Japanese', 20 ],
+  [ 'Australian', 18 ],
+  [ 'Dutch', 18 ],
+  [ 'Spanish', 15 ],
+  [ 'Austrian', 15 ],
+  [ 'Canadian', 14 ],
+  [ 'Swedish', 10 ],
+  [ 'Finnish', 9 ],
+  [ 'New Zealander', 9 ],
+  [ 'Mexican', 6 ],
+  [ 'Irish', 5 ],
+  [ 'Danish', 5 ],
+  [ 'Portuguese', 4 ],
+  [ 'Monegasque', 4 ],
+  [ 'Rhodesian', 4 ],
+  [ 'Uruguayan', 4 ],
+  [ 'Russian', 4 ],
+  [ 'Colombian', 3 ],
+  [ 'Venezuelan', 3 ],
+  [ 'East German', 3 ],
+  [ 'Indian', 2 ],
+  [ 'Thai', 2 ],
+  [ 'Polish', 1 ],
+  [ 'Hungarian', 1 ],
+  [ 'Czech', 1 ],
+  [ 'Malaysian', 1 ],
+  [ 'Chilean', 1 ],
+  [ 'Liechtensteiner', 1 ],
+  [ 'American-Italian', 1 ],
+  [ 'Argentine-Italian', 1 ],
+  [ 'Indonesian', 1 ],
+  [ 'Chinese', 1 ]
+]
+
+const data = waffle_data.filter(item => item[1] > 25).map(item => {
+  return { nationality: item[0], count: item[1] };
+});
 
 function App() {
   return (
