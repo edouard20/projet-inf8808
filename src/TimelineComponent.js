222f5f3c096cfae0f038bdc5e5e66db101ec4f42
edouard20 <edouardfrechette20@gmail.com>
2024-04-01 17:53:21 -0400

    added Fernando Alonso timeline

---
+++ src/TimelineComponent.js
@@ -0,0 +1,268 @@
+import React from 'react';
+import './Timeline.css';
+
+function Timeline() {
+
+    return (
+        <div class="timeline">
+        <div class="year">
+          <div class="inner">
+            <span>2001</span>
+          </div>
+        </div>
+
+        <ul class="days">          
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://i.redd.it/zzuq6zczaj871.jpg" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Fernando Alonso's first race at the 2001 Australian Grand Prix in a Minardi PS01</div>
+            </div>
+          </li>
+          
+          {/* <li class="day">
+            <div class="events">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
+              <div class="date">18 October (Monday)</div>
+            </div>
+          </li>
+          
+          <li class="day">
+            <div class="events">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
+              <div class="date">18 October (Monday)</div>
+            </div>
+          </li>
+          
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="http://placehold.it/400x300" alt="" />
+                <p class="caption">
+                  Lorem ipsum dolor sit amet.
+                </p>
+              </div>
+              <div class="date">18 October (Monday)</div>
+            </div>
+          </li> */}
+        </ul>
+        
+        <div class="year">
+          <div class="inner">
+            <span>2003</span>
+          </div>
+        </div>
+
+        <ul class="days">
+          {/* <li class="day">
+            <div class="events">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
+              <div class="date">18 October (Monday)</div>
+            </div>
+          </li>
+          
+          <li class="day">
+            <div class="events">
+              <p>Lorem dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
+              <div class="date">18 October (Monday)</div>
+            </div>
+          </li> */}
+          
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://preview.redd.it/k3f026wfard31.jpg?auto=webp&s=bad72f0b5658edae1f375a051d227079e0811273" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Fernando Alonso's first win at the 2003 Hungarian Grand Prix</div>
+            </div>
+          </li>
+          
+          {/* <li class="day">
+            <div class="events">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
+              <div class="date">18 October (Monday)</div>
+            </div>
+          </li>
+          
+          <li class="day">
+            <div class="events">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius perferendis vitae, facere accusantium magni, explicabo mollitia quidem odio autem, iste optio? Consequuntur ratione dolorum velit maiores quam odit odio suscipit.</p>
+              <div class="date">18 October (Monday)</div>
+            </div>
+          </li>
+           */}
+        </ul>
+        <div class="year">
+          <div class="inner">
+            <span>2005</span>
+          </div>
+        </div>
+        <ul class="days">
+        <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://media.formula1.com/content/dam/fom-website/manual/d05bra1308.jpg" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Fernando Alonso wins the 2005 F1 World Championship</div>
+            </div>
+          </li>
+        </ul>
+        <div class="year">
+          <div class="inner">
+            <span>2006</span>
+          </div>
+          <ul class="days">
+        <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://media.formula1.com/content/dam/fom-website/manual/d05bra1387.jpg" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Fernando Alonso and Renault retain both Championship Titles</div>
+            </div>
+          </li>
+        </ul>
+        </div>
+        <div class="year">
+          <div class="inner">
+            <span>2007</span>
+          </div>
+          <ul class="days">
+        <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://imgresizer.eurosport.com/unsafe/2560x1440/filters:format(jpeg)/origin-imgresizer.eurosport.com/2007/01/15/330800-24100760-2560-1440.jpg" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Fernando Alonso joins Mclaren to drive alongside rookie Lewis Hamilton</div>
+            </div>
+          </li>
+        </ul>
+        </div>
+        <div class="year">
+          <div class="inner">
+            <span>2008</span>
+          </div>
+          <ul class="days">
+        <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://img.rasset.ie/00015759-1600.jpg" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">After one season with Mclaren, Alonso rejoins Renault</div>
+            </div>
+          </li>
+        </ul>
+        </div>
+        <div class="year">
+          <div class="inner">
+            <span>2010</span>
+          </div>
+          <ul class="days">
+        <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://pbs.twimg.com/media/E7d6bdoVIAAABOE.jpg:large" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Fernando joins the team that all F1 drivers dream to race for, Ferrari</div>
+            </div>
+          </li>
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2016/09/07/14732440715670.jpg" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Across this 4-year stint with Ferrari, Fernando was able to battle for the title between 2010 <br></br>
+               and 2012 but sadly missed out on the title in the final races in 2010 and 2012</div>
+            </div>
+          </li>
+        </ul>
+        </div>
+        <div class="year">
+          <div class="inner">
+            <span>2015</span>
+          </div>
+          <ul class="days">
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://i.guim.co.uk/img/media/1bc3558c5b516622e0ce4569d3b823130532c85d/645_506_4010_2406/master/4010.jpg?width=1200&quality=85&auto=format&fit=max&s=4df1514fb37c138202f8267eeee6fdbc" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">With Lewis Hamilton moving to Mercedes, Fernando decides to make a return to McLaren</div>
+            </div>
+          </li>
+        </ul>
+        </div>
+        {/* <div class="year">
+          <div class="inner">
+            <span>2018</span>
+          </div>
+          <ul class="days">
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://library.sportingnews.com/styles/crop_style_16_9_desktop/s3/2022-06/sporting-news-2022-photo-with-watermark-f6b841ca-cadb-4c9e-9247-3d50d7fe9cad.png?itok=13h4x6cb" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">After a 2-year break, the legendary driver decides to make a return to Formula 1. <br></br> He joins his former Renault team, now rebranded as Alpine</div>
+            </div>
+          </li>
+        </ul>
+        </div> */}
+        <div class="year">
+          <div class="inner">
+            <span>2018</span>
+          </div>
+          <ul class="days">
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://fotografias.larazon.es/clipping/cmsimages01/2022/11/13/804255EC-DAE0-4F5B-B5B7-3D2F81C99053/98.jpg?crop=4601,2589,x0,y239&width=1900&height=1069&optimize=low&format=webply" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">After a 2-year break, the legendary driver decides to make a return to Formula 1. <br></br> He joins his former Renault team, now rebranded as Alpine</div>
+            </div>
+          </li>
+        </ul>
+        </div>
+        <div class="year">
+          <div class="inner">
+            <span>2023-Present</span>
+          </div>
+          <ul class="days">
+          <li class="day">
+            <div class="events">
+              <div class="day__img">
+                <img src="https://cdn.racingnews365.com/2023/Alonso/_1800x945_crop_center-center_75_none/Alonso-Aston-Martin-Australian-GP-2023.jpg?v=1680269435" alt="" />
+                <p class="caption">
+                </p>
+              </div>
+              <div class="date">Fernando joins the Aston Martin challenge. An unexpected force with an underdog mentality</div>
+            </div>
+          </li>
+        </ul>
+        </div>
+      </div>
+    )
+}
+
+export default Timeline;
