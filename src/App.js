4a90f124c6cc49dcb234e17b0b2b91fc3d697383
edouard20 <edouardfrechette20@gmail.com>
2024-04-01 15:22:40 -0400

    Ui cleanup

--- src/App.js
+++ src/App.js
@@ -7,28 +7,48 @@ import ImageAnimation from "./ImageAnimation";
 import data from './texts.json';
 
 function App() {
+
   const visualizations = [<TextSection/>, <TextSection/>, <TextSection/>, <TextSection/>, <TextSection/>] // Add visualizations here
   const items = data.texts;
   return (
     <>
       <ProgressBar/>
       <div className="App">
-        <h1 className='introduction-text'>Speed Through Time: The Evolution of Formula 1</h1>
+        <h1 className='title-text'>Speed Through Time: The Evolution of Formula 1</h1>
       </div>
+
       <TitleText title={"The Era of Dominance: Powerhouses of Formula 1"}/>
+      <div className="text-section">
+        <TextSection text={items[0]}/>
+      </div>
+
       <div className='images-box'>
           <ImageAnimation img={"https://cdn-4.motorsport.com/images/amp/0qAWQR80/s1000/formula-1-argentinian-gp-1981--2.jpg"} delay={0.5} duration={0.8}></ImageAnimation>
           <ImageAnimation img={"https://cdn.ferrari.com/cms/network/media/img/resize/5e33f7f70bd18308db1a854b-ferrari-scuderia-1991-alesi-prost-cover-mob?width=768&height=1024"} delay={1.5} duration={1.5} ></ImageAnimation>
           <ImageAnimation img={"https://www.creditplus.co.uk/media/6839/history-of-ferrari-in-f1-pit-stop.jpg"} delay={2.5} duration={2.5} ></ImageAnimation>
-        </div>
+      </div>
+
+      <TitleText title={"Racing Giants: The Dominant Nations of the Sport"}/>
       <div className="text-section">
-      {items.map((item, i) => (
-        <div>
-          <TextSection key={i} text={item}/>
-          {visualizations[i]}
-        </div>
-      ))}
-    </div>
+        <TextSection text={items[1]}/>
+      </div>
+
+      <TitleText title={"Iconic Circuits: The Heartbeat of Formula 1"}/>
+      <div className="text-section">
+        <TextSection text={items[2]}/>
+      </div>
+
+      <TitleText title={"Aged like Fine Wine: The Timeless Veterans of the Sport"}/>
+      <div className="text-section">
+        <TextSection text={items[3]}/>
+      </div>
+
+      <TitleText title={"The Dynamics of Competition: Analyzing Position Shifts in Races"}/>
+      <div className="text-section">
+        <TextSection text={items[4]}/>
+      </div>
+
+
     </>
 )};
 
