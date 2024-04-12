f9b8ea001cb90360beb8be7062779f5e65dd4c2a
Ahmed <a.hammami.u@gmail.com>
2024-04-11 22:22:47 -0400

    adjusting after barchart merge

--- src/App.js
+++ src/App.js
@@ -254,10 +254,10 @@ function App() {
                     width={'500px'}
                 ></ImageAnimation>
             </div>
-            
+
             <TitleText title={"F1 Teams/Drivers Dominance"}/>
             <div className="text-section">
-                <TextSection text={items[6]}/>
+                <TextSection text={items[5]}/>
             </div>
             <RankFlowChart data={f1_teams_data}></RankFlowChart>
 
