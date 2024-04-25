1fe2a0131ed4bc6795795f62ed66284177b99e54
AlexisLavigne <alexis.lavigne@polymtl.ca>
2024-04-25 13:33:21 -0400

    AL: deleted comments

--- src/ParallaxText.js
+++ src/ParallaxText.js
@@ -13,11 +13,7 @@ function ParallaxText({ children, baseVelocity = 100 }) {
   const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
     clamp: false
   });
-  /**
-   * This is a magic wrapping for the length of the text - you
-   * have to replace for wrapping that works for you or dynamically
-   * calculate
-   */
+
   const x = useTransform(baseX, (v) => `${wrap(0, -10, v)}%`);
 
   const directionFactor = useRef(1);
@@ -34,13 +30,7 @@ function ParallaxText({ children, baseVelocity = 100 }) {
 
     baseX.set(baseX.get() + moveBy);
   });
-  /**
-   * The number of times to repeat the child text should be dynamically calculated
-   * based on the size of the text and viewport. Likewise, the x motion value is
-   * currently wrapped between -20 and -45% - this 25% is derived from the fact
-   * we have four children (100% / 4). This would also want deriving from the
-   * dynamically generated number of children.
-   */
+
   return (
     <div className="parallax">
       <motion.div className="scroller" style={{ x }}>
